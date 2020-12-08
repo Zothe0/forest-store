a5946a45fd77ef88ae84ff39e8085c8ea9af6bb1
zothe <lex4prime@gmail.com>
2020-12-08 20:54:04 +0300

    Change mail port to 567

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,39 @@
 	<div v-bind:class="{ lock: menuVisible }" class="main-wrapper">
 		<menuBar />
 		<Nuxt />
+		<noscript
+			><div>
+				<img
+					src="https://mc.yandex.ru/watch/70222543"
+					style="position:absolute; left:-9999px;"
+					alt=""
+				/></div
+		></noscript>
 	</div>
 </template>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+;(function(m, e, t, r, i, k, a) {
+	m[i] =
+		m[i] ||
+		function() {
+			;(m[i].a = m[i].a || []).push(arguments)
+		}
+	m[i].l = 1 * new Date()
+	;(k = e.createElement(t)),
+		(a = e.getElementsByTagName(t)[0]),
+		(k.async = 1),
+		(k.src = r),
+		a.parentNode.insertBefore(k, a)
+})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym")
 
+ym(70222543, "init", {
+	clickmap: true,
+	trackLinks: true,
+	accurateTrackBounce: true,
+})
+</script>
+<!-- /Yandex.Metrika counter -->
 <style>
 html {
 	font-family: "Roboto", "Helvetica Neue", Arial, sans-serif;
@@ -21,7 +51,7 @@ html {
 	height: 100%;
 	position: relative;
 }
-body.lock{
+body.lock {
 	overflow: hidden;
 }
 </style>
@@ -34,7 +64,7 @@ export default {
 		return {
 			bodyAttrs: {
 				class: this.menuVisible ? "lock" : "",
-			}
+			},
 		}
 	},
 	computed: {
