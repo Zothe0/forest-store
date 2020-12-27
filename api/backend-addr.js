1c7f4869db6b4d81e40156beeb05dddd655f23ce
zothe <lex4prime@gmail.com>
2020-12-28 01:51:37 +0300

    Your Message

--- api/backend-addr.js
+++ api/backend-addr.js
@@ -1,3 +1,3 @@
-// const address = "http://185.193.143.164:3005"
-const address = "http://localhost:3005"
+const address = "http://185.193.143.164:3005"
+// const address = "http://localhost:3005"
 export default address
