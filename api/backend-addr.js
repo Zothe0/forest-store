7409b3cc6a511f02185ac39932d02ef22a939803
zothe <lex4prime@gmail.com>
2021-01-12 00:35:12 +0300

    Update

--- api/backend-addr.js
+++ api/backend-addr.js
@@ -1,4 +1,4 @@
-// const address = "https://lesnye-radosti.ru"
+const address = "https://lesnye-radosti.ru"
 // const address = "http://localhost:3001"
-const address = "http://185.193.143.164:3001"
+// const address = "http://185.193.143.164:3001"
 export default address
